7cce810b7ea77c633227ad90d38b093b31b1a14c
Sotiris Filippidis <sotirisf@dot-see.com>
2023-10-10 19:31:06 +0300

    Code (unusable for the time being)

---
+++ src/DotSee.Discipline/AutoNode/JsonFileRuleProviderService.cs
@@ -0,0 +1,76 @@
+﻿using Lucene.Net.Util.Fst;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+
+namespace DotSee.Discipline.AutoNode
+{
+    public class JsonFileRuleProviderService : IRuleProviderService<AutoNodeJsonRules>
+    {
+        private Dictionary<string, string> _settings;
+        private List<Rule> _rules;
+        private AutoNodeJsonRules _configType = null;
+        private readonly IConfigSource _configSource;
+        private readonly ILogger _logger;
+        private readonly IConfiguration _configuration;
+
+        public JsonFileRuleProviderService(ILogger logger, IConfigSource configSource, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configSource = configSource;
+            _settings = new Dictionary<string, string>();
+            _configuration = configuration;
+        }
+
+        public Dictionary<string, string> Settings
+        {
+            get
+            {
+                return (_settings == null || !_settings.Any()) ? this.ConfigType.Settings : _settings;
+            }
+        }
+
+        public IEnumerable<Rule> Rules
+        {
+            get
+            {
+                return (_rules == null || !_rules.Any()) ? GetRules() : _rules;
+            }
+        }
+
+        IConfigSource IRuleProviderService.ConfigSource { get => _configSource; }
+
+        public AutoNodeJsonRules ConfigType => _configType ?? GetConfigFromJson();
+
+        public void ReloadData()
+        {
+            _rules = null;
+            _settings = new Dictionary<string, string>();
+            _configType = null;
+        }
+
+        private List<Rule> GetRules()
+        {
+            List<Rule> r = new();
+
+            try
+            {
+                _configuration.GetSection("DotSee.Discipline:AutoNode:Rules").Bind(r);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, MessageConstants.ErrorLoadConfig);
+                return null;
+            }
+
+            _logger.Information(string.Format(MessageConstants.InfoLoadConfigComplete, r.Count));
+
+            return r;
+        }
+      
+    }
+}
---
+++ src/DotSee.Discipline/AutoNode/Rule.cs
@@ -0,0 +1,56 @@
+﻿namespace DotSee.Discipline.AutoNode
+{
+    /// <summary>
+    /// Holds rules for automatically creating nodes.
+    /// </summary>
+    public class Rule
+    {
+        public string CreatedDocTypeAlias { get; set; }
+        public string DocTypeAliasToCreate { get; set; }
+        public string NodeName { get; set; }
+        public bool? BringNewNodeFirst { get; set; }
+        public bool? OnlyCreateIfNoChildren { get; set; }
+        public bool? CreateIfExistsWithDifferentName { get; set; }
+        public string DictionaryItemForName { get; set; }
+        public bool? KeepNewNodeUnpublished { get; set; }
+        public string Blueprint { get; set; }
+
+        public Rule()
+        { }
+
+        /// <summary>
+        /// Creates a new rule for automatically creating nodes.
+        /// </summary>
+        /// <param name="createdDocTypealias">The document type Alias to look for. The rule will be applied when a document of this type is published.</param>
+        /// <param name="docTypeAliasToCreate">The document type to automatically create</param>
+        /// <param name="nodeName">The name of the newly created document</param>
+        /// <param name="bringNodeFirst">If this is set, then the node will be sorted in order to be first (as opposed to the default last position)</param>
+        /// <param name="onlyCreateIfNoChildren">If this is set to true, then the new node will only be created if the node published hasn't already got any child nodes.</param>
+        /// <param name="createIfExistsWithDifferentName">By default the rule does not create a node automatically only if another node of the same type with the same name already exists. By setting this parameter to false you can allow the creation of a node when one of the same type (but with a different name) already exists.</param>
+        /// <param name="dictionaryItemForName">Applies the node's name based on the given dictionary entry (default entry: AutoNode.Name)</param>
+        /// <param name="keepNewNodeUnpublished">Unpublishes the new node after creation</param>
+        /// <param name="blueprint">The name of a content template (blueprint) to apply to the newly created document</param>
+        public Rule(
+            string createdDocTypealias,
+            string docTypeAliasToCreate,
+            string nodeName,
+            bool? bringNodeFirst = false,
+            bool? onlyCreateIfNoChildren = false,
+            bool? createIfExistsWithDifferentName = true,
+            string dictionaryItemForName = "AutoNode.Name",
+            bool? keepNewNodeUnpublished = false,
+            string blueprint = ""
+            )
+        {
+            CreatedDocTypeAlias = createdDocTypealias;
+            DocTypeAliasToCreate = docTypeAliasToCreate;
+            NodeName = nodeName;
+            BringNewNodeFirst = bringNodeFirst;
+            OnlyCreateIfNoChildren = onlyCreateIfNoChildren;
+            CreateIfExistsWithDifferentName = createIfExistsWithDifferentName;
+            DictionaryItemForName = dictionaryItemForName;
+            KeepNewNodeUnpublished = keepNewNodeUnpublished;
+            Blueprint = blueprint;
+        }
+    }
+}
