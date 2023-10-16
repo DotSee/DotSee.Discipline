0da12a5785f9431dda827d26ea1f16aa4e42cc53
Sotiris Filippidis <sotirisf@dot-see.com>
2023-10-16 17:18:15 +0300

    project

---
+++ src/DotSee.Discipline/AutoNode/RuleSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotSee.Discipline.AutoNode
+{
+    public class RuleSettings
+    {
+        public string LogLevel { get; set; } = "Normal";
+        public bool RepublishExistingNodes { get; set; } = false;
+    }
+}
