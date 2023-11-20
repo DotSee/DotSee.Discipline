dbb1d3247f5a78e3d3de89077e72de2da10a9798
Stelios Zisis <szisis@dot-see.com>
2023-11-20 17:35:52 +0200

    ~ ignore smidge

---
+++ src/UmbracoV10/Smidge/Cache/SEEDEV6/64f6b05ec4ae1803b8e8c7b3dbc6129867bb6221/45bf6072.js
@@ -0,0 +1,1 @@
+function HideShowTreeNodes(){$("li.umb-tree-item.not-published").each(function(){tgl($(this))});$(document).arrive("li.umb-tree-item.not-published-add",function(){tgl($(this))})}function tgl(t){var name=t.find("a").text();name.startsWith("(")&&name.endsWith(")")&&t.toggle()}
