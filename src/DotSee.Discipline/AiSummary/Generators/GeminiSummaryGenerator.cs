827c0b5039adbc23bdc298914c48c28fe63eb882
Sotiris Filippidis <sotirisf@dot-see.com>
2026-01-07 13:52:01 +0200

    Fix multilinguals for Gemini summary generation

--- src/DotSee.Discipline/AiSummary/Generators/GeminiSummaryGenerator.cs
+++ src/DotSee.Discipline/AiSummary/Generators/GeminiSummaryGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using Google.GenAI;
+using Google.GenAI.Types;
 using System.Text;
 
 namespace DotSee.Discipline.AiSummary.Generators
@@ -19,9 +20,28 @@ namespace DotSee.Discipline.AiSummary.Generators
             promptBuilder.AppendLine($"Take all text under consideration.");
             promptBuilder.AppendLine($"Here is the text: {content}");
 
+            var config = new GenerateContentConfig
+            {
+                SystemInstruction = new Content
+                {
+                    Parts =
+                        new List<Part>
+                        {
+                            new Part
+                            {
+                                Text =
+                                @"Always respond in the SAME language as the input text.
+                                Never translate unless the user explicitly asks for translation.
+                                Identify the primary language of the text after the 'Here is the text:' part and create a summary in that language. Do not output a response for identification."
+                            }
+                        }
+                }
+            };
+
+
             var client = new Client(apiKey: apiKey);
 
-            var res = client.Models.GenerateContentAsync(model: aiModel, contents: promptBuilder.ToString());
+            var res = client.Models.GenerateContentAsync(model: aiModel, contents: promptBuilder.ToString(), config: config);
 
             return (res.Result.Candidates[0].Content.Parts[0].Text);
         }
