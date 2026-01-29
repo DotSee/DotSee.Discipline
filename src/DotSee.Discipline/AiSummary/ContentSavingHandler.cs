eaa63e9f25d161daa62ebedb1b438078b47def41
Sotiris Filippidis <sotirisf@dot-see.com>
2026-01-29 16:04:23 +0200

    ai summary logging

--- src/DotSee.Discipline/AiSummary/ContentSavingHandler.cs
+++ src/DotSee.Discipline/AiSummary/ContentSavingHandler.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Core.Events;
+using Serilog;
+using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Notifications;
 
@@ -7,11 +8,14 @@ namespace DotSee.Discipline.AiSummary
     public class ContentSavingHandler : INotificationHandler<ContentSavingNotification>
     {
         private readonly AiSummaryService _svc;
+        private readonly ILogger _logger;
 
-        public ContentSavingHandler(AiSummaryService svc)
+        public ContentSavingHandler(AiSummaryService svc, ILogger logger)
         {
             _svc = svc;
+            _logger = logger;
         }
+
         public void Handle(ContentSavingNotification notification)
         {
             foreach (IContent node in notification.SavedEntities)
@@ -23,6 +27,7 @@ namespace DotSee.Discipline.AiSummary
                 }
                 catch (Exception ex)
                 {
+                    _logger.Error(ex, MessageConstants.ErrorContentSaving, node.Id, node.Name);
                     notification.Messages.Add(new EventMessage(category: "AI Summary", "Something went wrong. AI summary was not updated. Please check your logs.", EventMessageType.Warning));
                 }
             }
